34ce5b5c128c5acad436fccfd5440a5e58c33aae
Aqib <aqibrime@gmail.com>
2024-05-03 06:04:32 +0600

    upgrade dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Counter from '@/components/client/Counter';
-
 export default function Home() {
   return (
     <main className="m-10">
       NextJs starter template {" "}
-      <Counter />
     </main>
   );
 }
--- src/components/client/Counter.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { Button } from '@/components/ui/button';
-import { useSignal } from '@preact/signals-react';
-
-export default function Counter() {
-  const count = useSignal(0);
-  return <Button onClick={() => (count.value += 1)}>Count is {count}</Button>;
-}
